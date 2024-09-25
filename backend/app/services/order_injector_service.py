0e7f1b3e3503e026acc3a216bb228f1785d2d148
juliano <thespamer@gmail.com>
2024-09-25 18:32:11 +0100

    first commit

--- backend/app/services/order_injector_service.py
+++ backend/app/services/order_injector_service.py
@@ -4,13 +4,16 @@ import asyncio
 from app.models.order import Order, OrderType, Side
 from app.services.order_service import OrderService
 from app.services.market_data_service import market_data_db
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 class OrderInjectorService:
     @staticmethod
     async def inject_orders():
         while True:
             if market_data_db:
-                # Selecionar um papel aleatório
+                # Selecionar um papel aleatório dentre todos os papéis criados
                 symbol = random.choice(list(market_data_db.keys()))
                 # Criar uma ordem de compra ou venda aleatória
                 order_type = random.choice([OrderType.market, OrderType.limit])
@@ -20,6 +23,9 @@ class OrderInjectorService:
                 
                 order = Order(symbol=symbol, quantity=quantity, order_type=order_type, side=side, price=price)
                 await OrderService.create_order(order)
+                
+                # Log para monitorar a criação de ordens
+                logging.info(f"New order created for {symbol}: {order}")
 
             # Esperar 5 segundos antes de gerar a próxima ordem
             await asyncio.sleep(5)
